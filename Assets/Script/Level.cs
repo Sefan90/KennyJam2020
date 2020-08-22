fedc543e9f44d106d639e1b8ae31bacc136f246b
Stefan <stevetv90@gmail.com>
2020-08-22 21:07:23 +0200

    New level and camera code

---
+++ Assets/Script/Level.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    public GameObject grid1;
+    public GameObject grid2;
+    public GameObject grid3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(0,1,0) * Time.deltaTime);
+
+        if(transform.position.y-grid1.transform.position.y > 10f)
+        {
+            grid1.transform.position = new Vector3(0f,grid1.transform.position.y+24f,0f);
+        }
+        else if(transform.position.y-grid2.transform.position.y > 10f)
+        {
+            grid2.transform.position = new Vector3(0f,grid2.transform.position.y+24f,0f);
+        }
+        else if(transform.position.y-grid3.transform.position.y > 10f)
+        {
+            grid3.transform.position = new Vector3(0f,grid3.transform.position.y+24f,0f);
+        }
+    }
+}
--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -6,10 +6,16 @@ public class Player : MonoBehaviour
 {
     private float speed = 3f;
     private float jumpForce = 7f;
+    private int layer;
+    private bool alive = true;
+
+    public GameObject maincamera;
+    float camheight;
+    float camwidth;
     // Start is called before the first frame update
     void Start()
     {
-
+        layer = gameObject.layer;
     }
 
     // Update is called once per frame
@@ -17,10 +23,17 @@ public class Player : MonoBehaviour
     {
         GetComponent<Rigidbody2D>().velocity = new Vector2(Input.GetAxisRaw("Horizontal")*speed, GetComponent<Rigidbody2D>().velocity.y); 
         
-        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W)) && (Physics2D.Raycast(transform.position + new Vector3(-0.5f,0,0), Vector2.down, 0.5f).collider != null
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W)) && 
+        (Physics2D.Raycast(transform.position + new Vector3(-0.5f,0,0), Vector2.down, 0.5f).collider != null
         || Physics2D.Raycast(transform.position + new Vector3(0.5f,0,0), Vector2.down, 0.5f).collider != null))
         { 
             GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpForce); 
-        } 
+            gameObject.layer += 1;
+            print(gameObject.layer);
+            if (gameObject.layer > 12)
+            {
+                gameObject.layer = layer;
+            }
+        }
     }
 }
